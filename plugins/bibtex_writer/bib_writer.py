b75582112825b943f6421bcddf93a18f14172294
Wouter Bulten <wouterbulten@mai-ai.org>
2018-05-28 14:25:38 +0200

    Dump full bibitem when author or title not present

--- plugins/bibtex_writer/bib_writer.py
+++ plugins/bibtex_writer/bib_writer.py
@@ -85,6 +85,7 @@ def generate_md_bibitem(pelican_object, writer=None):
         if 'author' not in global_index[bibitem].entry or 'title' not in global_index[bibitem].entry:
             # It skips bibitems with absence of authors or title
             print("no author for {}".format(bibitem))
+            print(global_index[bibitem])
             continue
 
         md_format += 'title: ' + global_index[bibitem].entry['title'] + '\n'
